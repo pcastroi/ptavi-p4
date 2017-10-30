8acc2b8311ce9a8fca16c238431a3c20f2284d36
Pablo Castro Ilundain <pilundai@gamma09.aulas.gsyc.urjc.es>
2017-10-30 13:25:35 +0100

    Acabado el punto 7

--- client.py
+++ client.py
@@ -11,15 +11,18 @@ LINE = sys.argv[3]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    if len(sys.argv) != 6:
+        sys.exit('Usage: client.py ip puerto register sip_address expires_value')
+        
     my_socket.connect((SERVER, PORT))
     if LINE == 'register':
         line = 'REGISTER sip:' + sys.argv[4] + ' SIP/2.0\r\n'
-        Expir = 'Expires: ' + sys.argv[5] + ' \r\n'
-        my_socket.send(bytes(line, 'utf-8') + b'\r\n')
+        expr = 'Expires: ' + sys.argv[5] + ' \r\n'
+        my_socket.send(bytes(line + expr, 'utf-8') + b'\r\n')
     else:
         sys.exit()
     
-    print('Enviando:', line)
+    print('Enviando:', line + expr + '\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
     
--- server.py
+++ server.py
@@ -3,23 +3,39 @@
 
 import socketserver
 import sys
+import time
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
-    resdic = {}
+
+    sipdic = {}
     def handle(self):
 
         print('IP cliente: ' + self.client_address[0] + '\t'
          + 'Puerto cliente: ' + str(self.client_address[1]))
+        listdel = []
         for line in self.rfile:
-            dcline = line.decode('utf-8')
+            decodlin = line.decode('utf-8')
+            print(decodlin)
             if not line:
                 continue
-            elif dcline.split(' ')[0] == 'REGISTER':
-                self.resdic[dcline.split(' ')[1]
-                [dcline.split(' ')[1].find(':') + 1 : ]] = self.client_address[0]
-                print("El cliente nos manda:", dcline)
-                print(self.resdic)
+            elif decodlin.split(' ')[0] == 'REGISTER':
+                sipusr = decodlin.split(' ')[1][decodlin.split(' ')[1].find(':') + 1 :] 
+                self.sipdic[sipusr] = [self.client_address[0], 0]
+            elif decodlin.split(' ')[0] == 'Expires:':
+                expt = float(decodlin.split(' ')[1]) + time.time()
+                #date = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+                self.sipdic[sipusr][1] = expt
+                for user in self.sipdic:
+                    if self.sipdic[user][1] <= time.time():
+                        listdel.append(user)
+                        print('Tiempo expirado')
+                    else:
+                        print('Tiempo no expirado')
+                
+                for user in listdel:    
+                    del self.sipdic[user]
+                    print('Diccionario:', self.sipdic)
                 self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
             else:
                 pass
