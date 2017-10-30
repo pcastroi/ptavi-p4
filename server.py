5216427a0322bfb84ecd6e979cb7799b6f91fa1d
Pablo Castro Ilundain <pilundai@gamma09.aulas.gsyc.urjc.es>
2017-10-30 14:04:01 +0100

    Acabado el punto 8

--- server.py
+++ server.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import json
 import socketserver
 import sys
 import time
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
-
     sipdic = {}
-    def handle(self):
+    
+    def register2json(self):
+        json.dump(self.sipdic, open('registered.json', 'w'))
 
+
+    def handle(self):
         print('IP cliente: ' + self.client_address[0] + '\t'
          + 'Puerto cliente: ' + str(self.client_address[1]))
         listdel = []
@@ -19,27 +23,31 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             print(decodlin)
             if not line:
                 continue
+                
             elif decodlin.split(' ')[0] == 'REGISTER':
                 sipusr = decodlin.split(' ')[1][decodlin.split(' ')[1].find(':') + 1 :] 
                 self.sipdic[sipusr] = [self.client_address[0], 0]
+                self.register2json()
+
             elif decodlin.split(' ')[0] == 'Expires:':
                 expt = float(decodlin.split(' ')[1]) + time.time()
-                #date = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-                self.sipdic[sipusr][1] = expt
+                self.sipdic[sipusr][1] = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(expt))
                 for user in self.sipdic:
-                    if self.sipdic[user][1] <= time.time():
+                    if self.sipdic[user][1] <= time.strftime('%Y-%m-%d %H:%M:%S',
+                     time.gmtime(time.time())):
                         listdel.append(user)
                         print('Tiempo expirado')
                     else:
                         print('Tiempo no expirado')
                 
-                for user in listdel:    
+                for user in listdel:
                     del self.sipdic[user]
                     print('Diccionario:', self.sipdic)
+                self.register2json()
                 self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
             else:
                 pass
-
+   
 if __name__ == "__main__":
 
     serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler) 
